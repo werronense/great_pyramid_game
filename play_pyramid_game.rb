7574f04103a1a177aef3c52f26abd86e00d4f845
werronense <31091965+werronense@users.noreply.github.com>
2019-08-07 13:31:58 -0400

    Update methods

--- play_pyramid_game.rb
+++ play_pyramid_game.rb
@@ -9,18 +9,38 @@ class Turn
 
   def initialize
     @@turn_number += 1
-    puts "Initial event" # test
+    print_game_state
   end
 
   def self.turn_count
     @@turn_number
   end
 
-  def print_state
+  def print_turn
     puts "Turn number #{@@turn_number}"
+  end
+
+  def print_resources
     puts "Available food: #{@@resources[:food]}"
     puts "Available manpower: #{@@resources[:manpower]}"
+  end
+
+  def print_pyramid
     puts "Pyramid status: #{@@pyramid_status * 100} percent built"
+  end
+
+  def print_game_state
+    print_turn
+    print_resources
+    print_pyramid
+  end
+
+  def run_event
+    event = Event.new(
+      @@resources[:food],
+      @@resources[:manpower],
+      @@pyramid_status
+    )
   end
 end
 
@@ -28,12 +48,17 @@ class Engine
   def play
     while Turn.turn_count < 10
       current_turn = Turn.new
-      current_turn.print_state
+      current_turn.run_event
     end
   end
 end
 
 class Event
+  def initialize(food, manpower, pyramid_status)
+    @food = food
+    @manpower = manpower
+    @pyramid_status = pyramid_status
+  end
 end
 
 # test
